cc8778d648e30b5b5b7d07e95d6953df8bb87454
Jashema Panapa <jcpanapa@gmail.com>
2020-04-05 20:04:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 # Make your shoe class here!
 
 class Shoe 
+  attr_accessor :color, :size, :material, :condition
   def initialize(brand)
     @brand = brand 
   end 
@@ -9,5 +10,4 @@ class Shoe
     @brand 
   end 
 
-attr_accessor :color, :size, :material, :condition
 end 
